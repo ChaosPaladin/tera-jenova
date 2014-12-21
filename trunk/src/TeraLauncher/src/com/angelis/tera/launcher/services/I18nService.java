b49b3051b3ef4e4b19dffdf7804f95485d74e9db
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-21 10:59:02 +0000

    TeraLauncher import

---
+++ trunk/src/TeraLauncher/src/com/angelis/tera/launcher/services/I18nService.java
@@ -0,0 +1,28 @@
+package com.angelis.tera.launcher.services;
+
+import com.angelis.tera.common.services.BaseI18nService;
+
+public class I18nService extends BaseI18nService {
+    
+    @Override
+    public void onInit() {
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+    
+    @Override
+    public String getBundleName() {
+        return "com.angelis.tera.launcher.i18n.messages";
+    }
+    
+    /** SINGLETON */
+    public static I18nService getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    private static class SingletonHolder {
+        protected static final I18nService instance = new I18nService();
+    }
+}
