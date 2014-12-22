45d22c36ab8983629cfafc7cb30d48529e0351ab
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-22 23:14:11 +0000

    - Maven refacto

--- src/TeraLogin/src/main/java/com/angelis/tera/login/process/services/NetworkService.java
+++ src/TeraLogin/src/main/java/com/angelis/tera/login/process/services/NetworkService.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 
 import com.angelis.tera.common.network.config.ServerConfig;
-import com.angelis.tera.common.network.http.ServerRunner;
 import com.angelis.tera.common.services.AbstractService;
 import com.angelis.tera.login.config.NetworkConfig;
+import com.angelis.tera.login.http.ServerRunner;
 import com.angelis.tera.login.network.TeraLoginServer;
 
 public class NetworkService extends AbstractService {
