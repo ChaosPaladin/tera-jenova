5cf33b729519482e329c37fae0592d053a127f79
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-22 14:46:20 +0000

    Thx svn ...

--- trunk/src/TeraGame/src/main/java/com/angelis/tera/game/command/admin/AbstractAdminCommand.java
+++
@@ -1,7 +0,0 @@
-package com.angelis.tera.game.command.admin;
-
-import com.angelis.tera.game.command.AbstractCommand;
-
-public abstract class AbstractAdminCommand extends AbstractCommand {
-    public abstract int getAccessLevel();
-}
--- trunk/src/TeraGame/src/main/java/com/angelis/tera/game/process/model/creature/enums/AiTypeEnum.java
+++
@@ -1,6 +0,0 @@
-package com.angelis.tera.game.process.model.creature.enums;
-
-public enum AiTypeEnum {
-    RANDOM,
-    PATH
-}
