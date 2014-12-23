9ec99303a99401d9f1202192c5ff2b0795cc4727
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-23 11:36:15 +0000

    - Maven refacto

--- src/TeraBuilder/build.bat
+++ src/TeraBuilder/build.bat
@@ -1,3 +1,11 @@
 @echo off
 
+if not defined JAVA_HOME (goto :askEnvironmentVar) else (goto :maven)
+
+:askEnvironmentVar
+set /p javahome=Enter JAVA_HOME path (jdk folder): 
+SET JAVA_HOME=%javahome%
+goto :maven
+
+:maven
 call mvn clean compile package
