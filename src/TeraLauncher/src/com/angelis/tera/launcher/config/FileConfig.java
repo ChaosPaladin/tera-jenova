9363026fd2df12a7b372a9420965f3bf9c8cb389
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-23 11:30:40 +0000

    thx svn again...

--- src/TeraLauncher/src/com/angelis/tera/launcher/config/FileConfig.java
+++
@@ -1,8 +0,0 @@
-package com.angelis.tera.launcher.config;
-
-import com.angelis.tera.common.config.Property;
-
-public class FileConfig {
-    @Property(key = "file.tera.folder", defaultValue = "C:\\Program Files (x86)\\TERA\\")
-    public static String TERA_FOLDER;
-}
