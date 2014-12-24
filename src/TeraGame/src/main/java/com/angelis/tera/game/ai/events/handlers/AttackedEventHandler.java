80c288fb8dc2ce2613601c3f6e0f868f86b61874
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-24 13:43:56 +0000

    - Some opcodes correction - Exchange stock items !

--- src/TeraGame/src/main/java/com/angelis/tera/game/ai/events/handlers/AttackedEventHandler.java
+++ src/TeraGame/src/main/java/com/angelis/tera/game/ai/events/handlers/AttackedEventHandler.java
@@ -1,19 +1,19 @@
 package com.angelis.tera.game.ai.events.handlers;
 
 import com.angelis.tera.game.ai.AI;
-import com.angelis.tera.game.ai.events.Event;
-import com.angelis.tera.game.ai.events.IEventHandler;
+import com.angelis.tera.game.ai.events.AIEventEnum;
+import com.angelis.tera.game.ai.events.IAIEventHandler;
 import com.angelis.tera.game.ai.state.enums.AIStateEnum;
 
-public class AttackedEventHandler implements IEventHandler {
+public class AttackedEventHandler implements IAIEventHandler {
 
     @Override
-    public Event getEvent() {
-        return Event.ATTACKED;
+    public AIEventEnum getEvent() {
+        return AIEventEnum.ATTACKED;
     }
 
     @Override
-    public void handleEvent(final Event event, final AI<?> ai) {
+    public void handleEvent(final AIEventEnum event, final AI<?> ai) {
         ai.setAiState(AIStateEnum.ATTACKING);
         if (!ai.isScheduled()) {
             ai.analyzeState();
