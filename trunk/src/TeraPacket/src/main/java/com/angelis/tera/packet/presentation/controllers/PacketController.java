172eca31116f3601fe31987aaa429adba5728eb8
angelis.projects@gmail.com <angelis.projects@gmail.com@f1fa2e4f-7cc9-b0fc-4b68-2a085ed95716>
2014-12-26 18:38:41 +0000

    TeraPacket better handling

---
+++ trunk/src/TeraPacket/src/main/java/com/angelis/tera/packet/presentation/controllers/PacketController.java
@@ -0,0 +1,125 @@
+package com.angelis.tera.packet.presentation.controllers;
+
+import java.io.IOException;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
+
+import com.angelis.tera.common.network.packet.AbstractClientPacket;
+import com.angelis.tera.common.network.packet.AbstractServerPacket;
+import com.angelis.tera.game.presentation.network.connection.TeraGameConnection;
+import com.angelis.tera.game.presentation.network.packet.ClientPacketHandler;
+import com.angelis.tera.game.presentation.network.packet.ServerPacketHandler;
+import com.angelis.tera.packet.process.network.packet.Packet;
+
+public class PacketController {
+
+    @FXML
+    private TextField filterTextField;
+
+    @FXML
+    private ListView<Packet> listView;
+
+    @FXML
+    private TextArea packetContentTextArea;
+
+    public PacketController() {
+
+    }
+
+    public void setFilterTextField(final TextField filterTextField) {
+        this.filterTextField = filterTextField;
+    }
+
+    public void setListView(final ListView<Packet> listView) {
+        this.listView = listView;
+    }
+
+    public ListView<Packet> getListView() {
+        return listView;
+    }
+
+    public TextField getFilterTextField() {
+        return filterTextField;
+    }
+
+    public void loadUi() {
+        listView.setCellFactory(new Callback<ListView<Packet>, ListCell<Packet>>() {
+            @Override
+            public ListCell<Packet> call(final ListView<Packet> p) {
+                final ListCell<Packet> cell = new ListCell<Packet>() {
+                    @Override
+                    protected void updateItem(final Packet packet, final boolean empty) {
+                        super.updateItem(packet, empty);
+                        if (packet != null) {
+
+                            final HBox hbox = new HBox();
+                            String packetName = null;
+                            String imageFile = null;
+                            switch (packet.getPacketDirection()) {
+                                case CLIENT_TO_SERVER:
+                                    final Class<? extends AbstractClientPacket<TeraGameConnection>> clientPacket = ClientPacketHandler.getClientPacket(packet.getOpcode());
+                                    if (clientPacket != null) {
+                                        packetName = clientPacket.getSimpleName();
+                                        imageFile = "fromClient.png";
+                                    }
+                                    else {
+                                        imageFile = "fromClientErr.png";
+                                    }
+                                break;
+
+                                case SERVER_TO_CLIENT:
+                                    final Class<? extends AbstractServerPacket<TeraGameConnection>> serverPacket = ServerPacketHandler.getServerPacket(packet.getOpcode());
+                                    if (serverPacket != null) {
+                                        packetName = serverPacket.getSimpleName();
+                                        imageFile = "fromServer.png";
+                                    }
+                                    else {
+                                        imageFile = "fromServerErr.png";
+                                    }
+                                break;
+                            }
+
+                            final ObservableList<Node> children = hbox.getChildren();
+                            try {
+                                children.add(new ImageView(new Image(getClass().getResource("/com/angelis/tera/packet/presentation/images/" + imageFile).openStream())));
+                            }
+                            catch (final IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            children.add(new Label(String.format(" 0x%02X ", packet.getOpcode())));
+                            if (packetName != null) {
+                                children.add(new Label(packetName));
+                            }
+
+                            setGraphic(hbox);
+                        }
+                    }
+                };
+
+                return cell;
+            }
+        });
+        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Packet>() {
+            @Override
+            public void changed(final ObservableValue<? extends Packet> observable, final Packet oldValue, final Packet newValue) {
+                if (newValue != null) {
+                    packetContentTextArea.setText(newValue.toString());
+                }
+            }
+        });
+    }
+}
